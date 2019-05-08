b9647790f098a72c49fdb2a21f90b4c9043e9ffb
Bruce Tian <tianrunhe@gmail.com>
2019-05-07 21:20:44 -0700

    Improve LCP

--- src/com/lintcode/problems/LongestCommonPrefix.java
+++ src/com/lintcode/problems/LongestCommonPrefix.java
@@ -6,21 +6,19 @@ import java.util.stream.IntStream;
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        final int longestLength = Arrays.stream(strs)
+        final int shortestLength = Arrays.stream(strs)
                 .mapToInt(String::length)
-                .max()
+                .min()
                 .orElse(0);
-        return IntStream.rangeClosed(0, longestLength)
+        return IntStream.rangeClosed(1, shortestLength)
                  .boxed()
-                 .map(i -> longestLength + 1 - i)
+                 .map(i -> shortestLength + 1 - i)
                  .map(size -> Arrays.stream(strs)
-                                    .filter(str -> str.length() >= size)
                                     .map(str -> str.substring(0, size))
-                                    .collect(Collectors.toList())
+                                    .collect(Collectors.toSet())
                      )
-                 .filter(candidates -> candidates.size() == strs.length)
-                 .filter(candidates -> candidates.stream().distinct().count() == 1)
-                 .map(list -> list.get(0))
+                 .filter(candidates -> candidates.size() == 1)
+                 .map(set -> set.iterator().next())
                  .findFirst()
                  .orElse("");
     }
